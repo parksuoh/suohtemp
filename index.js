693f8e094d8e4ea20a35e255b15b7a0e4dae958b
SUOH <sooho0163@naver.com>
2021-06-17 03:24:24 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser')
+const { User } = require("./models/User")
+
+const config = require('./config/key')
+
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.use(bodyParser.json())
+
+
+
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://suoh:abc123456789@shop1.g0zie.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connectd...'))
 .catch(err => console.log(err))
@@ -12,8 +24,31 @@ mongoose.connect('mongodb+srv://suoh:abc123456789@shop1.g0zie.mongodb.net/myFirs
 // mongoURI:'mongodb+srv://suoh:abc123456789@shop1.g0zie.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('와우 노드!')
 })
+
+
+app.post('/register', (req, res) => {
+
+    //회원가입 할떄 필요한 정보들을 client에서 가져오면
+    // 그것들을 데이터 베이스에 넣어준다.
+
+
+    const user = new User(req.body)
+
+    user.save((err, userInfo) => {
+        if(err) return res.json({ success: false, err})
+        return res.status(200).json({
+            succes: true
+        })
+    })
+
+})
+
+
+
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
